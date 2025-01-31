86e13c337e01a5da657ba62bd66880bd791893f9
BernardoAugus <bernardo.11salgado@hotmail.com>
2025-01-31 20:55:29 -0300

    first commit

---
+++ src/components/FloatButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page-sticky position="bottom-right" :offset="[18, 18]">
+    <q-fab icon="add" direction="up" color="accent">
+      <q-fab-action @click="onClick" color="primary" icon="person_add" />
+      <q-fab-action @click="onClick" color="primary" icon="mail" />
+    </q-fab>
+  </q-page-sticky>
+</template>
+
+<script setup lang="ts">
+onclick
+</script>
